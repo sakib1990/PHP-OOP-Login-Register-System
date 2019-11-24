f4f9571b7fe547b7f9d4926133087f12e3e529e1
sakib <ahmadsakibabdullah@gmail.com>
2019-11-24 11:45:51 +0600

    successfully added.

---
+++ inc/footer.php
@@ -0,0 +1,9 @@
+<div class="well">
+        <h3>Website: https://it-training4u.com
+            <span class="pull-right">Like Us: www.facebook.com/prosakib </span>
+        </h3>
+    </div>
+</div>
+
+</body>
+</html>
